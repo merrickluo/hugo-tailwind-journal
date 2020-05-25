61b874bb04e7a52f3934638719f5050981a22c85
A.I <merrick@luois.me>
2020-05-25 14:39:52 +0800

    add a togglable color scheme

---
+++ static/js/theme.js
@@ -0,0 +1,37 @@
+const detectTheme = () => {
+  let theme = localStorage.getItem("theme");
+
+  if (
+    !theme &&
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: light)").matches
+  ) {
+    theme = "light";
+  }
+
+  if (theme === "light") {
+    document.querySelector(".theme-toggle").classList.add("light");
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+};
+
+const switchTheme = e => {
+  const toggle = e.target;
+  let theme = document.documentElement.getAttribute("data-theme") || "dark";
+  if (theme === "light") {
+    e.target.classList.remove("light");
+    theme = "dark";
+  } else {
+    e.target.classList.add("light");
+    theme = "light";
+  }
+  localStorage.setItem("theme", theme);
+  document.documentElement.setAttribute("data-theme", theme);
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  detectTheme();
+  document
+    .querySelector(".theme-toggle")
+    .addEventListener("click", switchTheme);
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,33 @@ module.exports = {
   theme: {
     extend: {
       flex: {
-        'l-24': '0 0 6rem'
+        "l-24": "0 0 6rem"
       },
       fontFamily: {
-        serif: ['Arvo'],
-        mono: ['Source Code Pro']
+        serif: ["Arvo"],
+        mono: ["Source Code Pro"]
       },
       padding: {
-        '1/2': '0.125rem'
+        "1/2": "0.125rem"
+      },
+      colors: {
+        background: "var(--color-background)",
+        "background-alt": "var(--color-background-alt)",
+        "background-dark": "var(--color-background-dark)",
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        black: "var(--color-black)",
+        red: "var(--color-red)",
+        green: "var(--color-green)",
+        yellow: "var(--color-yellow)",
+        blue: "var(--color-blue)",
+        magenta: "var(--color-magenta)",
+        cyan: "var(--color-cyan)",
+        white: "var(--color-white)"
+      },
+      screens: {
+        "dark-mode": { raw: "(prefers-color-scheme: dark)" }
       }
     }
-  },
-  variants: {},
-  plugins: [],
-}
+  }
+};
